4eb4d99836e22ee81c6ff5dadcdd2d326038b13e
Mitsurugi <ha.bok.hada@gmail.com>
2017-02-03 11:47:37 -0600

    Primer "commit"
    el primer "release" .

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+  <title>Pagina Principal BT-Tech Idea Labs <?php echo date("Y"); ?></title>
+
+  <link rel="stylesheet" href="./tema.css">
+  <link rel="icon" href="images/favicon.png">
+</head>
+
+<body>
+<div id="body">
+  <div id="logo"></div>
+  <menu id="menu"><ul><li>Home</li><li>Acerca</li><li>Proyectos</li><li>Contacto</li></ul><menu>
+    <div id="content">
+      <div id="construccion">
+      <br><br><br>    <br><br><br>    <br><br><br>  Estamos en <-Construccion-> ... espera para ver ... nuestra nueva casa.
+      </div>
+    </div>
+    <div id="footer">Copyright (c) <?php echo date("Y"); ?> Copyright Imagenes y logos son de sus propios fabricantes.</div>
+</div>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+  <script src="js/script.js"></script>
+
+</body>
+
+</html>
